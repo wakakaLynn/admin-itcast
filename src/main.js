f687a014cdd63a328fac5fda80004835d8cc36d0
Lynn <418295062@qq.com>
2018-07-03 18:29:55 +0800

    首页整体布局

--- src/main.js
+++ src/main.js
@@ -12,6 +12,20 @@ Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+//注册一个全局守卫，作用是在路由跳转前，对路由进行判断，防止未登录的用户跳转到其他页面去
+router.beforeEach((to,from,next)=>{
+  let token = localStorage.getItem('mytoken')
+  if(token){
+    next()
+  }else{
+    if(to.path!=='/login'){
+      next({path:'/login'})
+    } else{
+      next()
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    登录成功
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 <script>
@@ -20,6 +27,44 @@
     }
   }
 </script>
-<style>
+<style lang="scss" scoped>
+  .home {
+    height: 100%;
+    .el-menu-admin:not(.el-menu--collapse) {
+      width: 200px;
+      min-height: 400px;
+    }
+    .el-container {
+      height: 100%;
+    }
+    .el-aside {
+      background-color: #545c64;
+    }
+    .el-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #545c64;
+    }
+    .logo {
+      height:60px;
+      background: url(../assets/logo.png);
+      background-size: cover;
+      background-color: #989898;
+    }
+    .toggle-btn {
+      font-size: 36px;
+      color: #989898;
+      cursor: pointer;
+      line-height: 60px;
+    }
+    .system-title {
+      font-size: 28px;
+      color: white;
+    }
+    .welcome, {
+      color: white;
+    }
+  }
 
 </style>
